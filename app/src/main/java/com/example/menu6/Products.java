414a299cafb65954b803e3cb2e2d28e76627337f
ep777 <ep777@MAIBENBEN-PC>
2024-12-12 05:23:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/menu6/Products.java
@@ -0,0 +1,16 @@
+package com.example.menu6;
+public class Products {
+    private String description;
+    private String title;
+
+    public Products(String description,String title) {
+        this.description = description;
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getTitle() {
+        return title;
+    }
+}
